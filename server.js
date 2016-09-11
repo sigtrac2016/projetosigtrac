4eaac1f3be6205778bf1befa098b3860a62b492c
ftuyama <ftuyama@gmail.com>
2016-09-11 14:40:04 -0300

    TS#01 Mudança diretório

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-//Store all HTML files in view folder.
-app.use(express.static(__dirname + '/views'));
-//Store all JS in Scripts folder.
-app.use(express.static(__dirname + '/script'));
-//Store all CSS in style folder.
-app.use(express.static(__dirname + '/style'));
-//Store all static files
-app.use(express.static(__dirname + '/public'));
+// Store all HTML files in view folder.
+app.use(express.static(__dirname + '/web/views'));
+// Store all JS in Scripts folder.
+app.use(express.static(__dirname + '/web/script'));
+// Store all CSS in style folder.
+app.use(express.static(__dirname + '/web/style'));
+// Store all static files
+app.use(express.static(__dirname + '/web/public'));
+// Store web path
+app.use(express.static(__dirname + '/web'));
 
 app.get('/', function(req, res) {
-    res.send(fs.readFileSync("views/main.html", "utf8"));
+    res.send(fs.readFileSync("web/views/main.html", "utf8"));
 })
 
 // set the port of our application
