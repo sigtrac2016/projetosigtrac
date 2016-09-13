2d45bae1b35a7d5a30bb3179324b277bace4a091
ftuyama <ftuyama@gmail.com>
2016-09-13 08:59:41 -0300

    Atualização das dependências

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function(req, res) {
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8081;
 
 var server = app.listen(port, function() {
     var host = server.address().address
