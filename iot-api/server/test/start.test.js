84d9bbd362231d3469ebf006edd5599d748fb867
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2016-09-21 01:41:59 -0300

    Re-implementar testes parametrizados sem mocha-each.

--- iot-api/server/test/start.test.js
+++ iot-api/server/test/start.test.js
@@ -19,74 +19,73 @@ const PRETO = 'Preto';
 describe('Vitimas', function() {
 
     describe ('API Pairwise', function() {
-        forEach([
-            [1, true, 20, true, 2, true, true],
-            [2, false, 40, false, 4, false, true],
-            [3, false, 20, false, 2, false, false],
-            [4, true, 40, true, 4, true, false],
-            [5, true, 40, false, 2, true, false],
-            [6, false, 20, true, 4, true, false],
-            [7, true, 20, true, 4, false, false]
-        ])
-        .it('post experimento%d', (exp, deambulando, respiracao, respiraViasAereas, reenchimentoCapilar, pulsoRadial, cumpreOrdens, done) => {
-            fetchData(exp, deambulando, respiracao, respiraViasAereas, reenchimentoCapilar, pulsoRadial, cumpreOrdens)
-                .then(
-                    request({
-                        url: requestURL.resolve(baseURL, vitimasAPI),
-                        method: 'POST',
-                        body: {
-                            alias: 'experimento' + exp,
-                            name: 'Teste 001',
-                            timeCreated: '2016-09-16',
-                            location: {
-                                latitude: -23.0,
-                                longitude: -45.0
-                            },
-                            deambulando: deambulando,
-                            respiracao: respiracao,
-                            respiraViasAereas: respiraViasAereas,
-                            reenchimentoCapilar: reenchimentoCapilar,
-                            pulsoRadial: pulsoRadial,
-                            cumpreOrdens: cumpreOrdens
+        var tests = [
+            {exp: 1, deambulando: true, respiracao: 20, respiraViasAereas: true, reenchimentoCapilar: 2, pulsoRadial: true, cumpreOrdens: true},
+            {exp: 2, deambulando: false, respiracao: 40, respiraViasAereas: false, reenchimentoCapilar: 4, pulsoRadial: false, cumpreOrdens: true},
+            {exp: 3, deambulando: false, respiracao: 20, respiraViasAereas: false, reenchimentoCapilar: 2, pulsoRadial: false, cumpreOrdens: false},
+            {exp: 4, deambulando: true, respiracao: 40, respiraViasAereas: true, reenchimentoCapilar: 4, pulsoRadial: true, cumpreOrdens: false},
+            {exp: 5, deambulando: true, respiracao: 40, respiraViasAereas: false, reenchimentoCapilar: 2, pulsoRadial: true, cumpreOrdens: false},
+            {exp: 6, deambulando: false, respiracao: 20, respiraViasAereas: true, reenchimentoCapilar: 4, pulsoRadial: true, cumpreOrdens: false},
+            {exp: 7, deambulando: true, respiracao: 20, respiraViasAereas: true, reenchimentoCapilar: 4, pulsoRadial: false, cumpreOrdens: false}
+        ];
+
+        tests.forEach(function(data) {
+            it('post experimento%d', function(done) {
+                request({
+                    url: requestURL.resolve(baseURL, vitimasAPI),
+                    method: 'POST',
+                    body: {
+                        alias: 'experimento' + data.exp,
+                        name: 'Teste 001',
+                        timeCreated: '2016-09-16',
+                        location: {
+                            latitude: -23.0,
+                            longitude: -45.0
                         },
-                        json: true
-                    }, function(error, response, body) {
-                        should.not.exist(error);
-                        should.exist(response);
-                        response.statusCode.should.equal(201);
-                        done();
-                    })
-                )
-                .then(done);
+                        deambulando: data.deambulando,
+                        respiracao: data.respiracao,
+                        respiraViasAereas: data.respiraViasAereas,
+                        reenchimentoCapilar: data.reenchimentoCapilar,
+                        pulsoRadial: data.pulsoRadial,
+                        cumpreOrdens: data.cumpreOrdens
+                    },
+                    json: true
+                }, function(error, response, body) {
+                    should.not.exist(error);
+                    should.exist(response);
+                    response.statusCode.should.equal(200);
+                    done();
+                });
+            });
         });
 
         context('Validação', () => {
-            forEach([
-                [1, VERDE],
-                [2, VERDE],
-                [3, VERDE],
-                [4, VERDE],
-                [5, VERDE],
-                [6, VERDE],
-                [7, VERDE]
-            ])
-            .it('get experimento%d', (exp, status, done) => {
-                fetchData(exp, status)
-                    .then(
-                        request({
-                            url: requestURL.resolve(baseURL, vitimasAPI + '/experimento' + exp),
-                            method: 'GET'
-                        }, function(error, response, body) {
-                            should.not.exist(error);
-                            should.exist(response);
-                            response.statusCode.should.equal(200);
-                            should.exist(body);
-                            var responseBody = JSON.parse(body);
-                            console.log('Request: %s, expected: %s', responseBody['status'], status);
-                            responseBody['status'].should.equal(status);
-                        })
-                    )
-                    .then(done);
+            var tests = [
+                {exp: 1, status: VERDE},
+                {exp: 2, status: VERDE},
+                {exp: 3, status: VERDE},
+                {exp: 4, status: VERDE},
+                {exp: 5, status: VERDE},
+                {exp: 6, status: VERDE},
+                {exp: 7, status: VERDE},
+            ];
+
+            tests.forEach(function(data) {
+                it('get experimento%d', function(done) {
+                    request({
+                        url: requestURL.resolve(baseURL, vitimasAPI + '/experimento' + data.exp),
+                        method: 'GET'
+                    }, function(error, response, body) {
+                        should.not.exist(error);
+                        should.exist(response);
+                        response.statusCode.should.equal(200);
+                        should.exist(body);
+                        var responseBody = JSON.parse(body);
+                        console.log('Request: %s, expected: %s', responseBody['status'], data.status);
+                        responseBody['status'].should.equal(data.status);
+                        done();
+                    });
+                });
             });
         });
 
