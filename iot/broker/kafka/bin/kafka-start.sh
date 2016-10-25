1a767d841cd7508bb153ed6002a9f206f1187c3c
SamaraCardoso27 <samaracardosodossantos@gmail.com>
2016-10-25 00:09:08 -0200

    Adicionar o app e remoçaõ de alguns arquivos

--- iot/broker/kafka/bin/kafka-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-_SERVER_PROPERTIES=/usr/local/kafka/config/server.properties
-
-[ "${ADVERTISED_HOSTNAME}" ] && _HOSTNAME=${ADVERTISED_HOSTNAME} || _HOSTNAME=${HOSTNAME}
-
-sed -i "s!^advertised.host.name=.*!advertised.host.name=${_HOSTNAME}!" ${_SERVER_PROPERTIES}
-
-exec /usr/local/kafka/bin/kafka-server-start.sh ${_SERVER_PROPERTIES} 
