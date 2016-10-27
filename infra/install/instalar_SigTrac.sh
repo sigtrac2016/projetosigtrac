dbd56f47ecde870712e6ec4d82ceb04bcb30ce5b
Marcelo Gumercino Costa <mgc@MacBook-Pro-de-Marcelo.local>
2016-10-27 17:01:45 -0200

    mudanças no script

--- infra/install/instalar_SigTrac.sh
+++ infra/install/instalar_SigTrac.sh
@@ -110,3 +110,14 @@ if [ $SERVER = "BANCO_DADOS" ];then
 	service mongodb start
 
 fi
+
+
+
+if [ $SERVER = "BROKER" ] || [ $SERVER = "API_WEB" ] ;then
+	apt-get install apt-transport-https ca-certificates
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+	echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+	aptitude update
+	aptitude install docker-engine docker.io
+fi
+
