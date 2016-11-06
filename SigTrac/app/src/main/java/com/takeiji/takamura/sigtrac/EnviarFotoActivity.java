d7640ecb1c84217053b123ca092376f01112a737
joseneto12 <joseneto06@gmail.com>
2016-11-06 15:48:21 -0200

    Update EnviarFotoActivity.java

--- SigTrac/app/src/main/java/com/takeiji/takamura/sigtrac/EnviarFotoActivity.java
+++ SigTrac/app/src/main/java/com/takeiji/takamura/sigtrac/EnviarFotoActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_enviar_foto);
 
         click = (Button) findViewById(R.id.camera);
         result = (ImageView) findViewById(R.id.imageView);
