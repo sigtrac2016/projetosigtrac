aa4d7756b8997f35cd86142a0885345e32712112
LuizFelipeh <luizfelipeh95@gmail.com>
2016-11-21 19:40:01 -0200

    Added crud funcionalities

--- iot-api/server/src/routes/crud.js
+++ iot-api/server/src/routes/crud.js
@@ -1,12 +1,13 @@
 //Basic Crud template
 var _ = require('lodash');
-//var mongoose = require('mongoose');
+var mongoose = require('mongoose');
+/*var User = require('../models/user.js')*/
 
 function readRequest(object, Model, req) {
 	var Attributes = _.keys(Model.schema.paths);
-
+	
 	_.forEach(Attributes, function(path){
-		var value = _.get(req.body, path);
+		value = _.get(req.body, path);
 		if(!_.isUndefined(value)){
 			_.set(object, path, value);
 		}
@@ -15,10 +16,22 @@ function readRequest(object, Model, req) {
 	return object;
 }
 
+function readQuery(object, Model, query) {
+	var Attributes = _.keys(Model.schema.paths);
+	_.forEach(Attributes, function(path){
+		value = _.get(query, path);
+		if(!_.isUndefined(value)){
+			_.set(object, path, JSON.parse(value));
+		}
+	});
+
+	return object;
+}
+
 function saveModel(object, res){
-	object.save(function(err/*, saved*/) {
+	object.save(function(err, saved) {
 		if(err){
-			console.log(err);
+			console.log(err)
 			res.send(err);
 		}
 		else {
@@ -29,13 +42,14 @@ function saveModel(object, res){
 }
 
 function findObject(Model, param, uniqueIdentifier, res, cb){
+	var returnObject;
 	if (_.isUndefined(uniqueIdentifier)){
-		Model.findById(param, function(err, object){
+		Model.findById(param).exec(function(err, object){
 			if (err)
 			  res.send(err);
-
+		
 			cb(object);
-		});
+			});			
 	}
 	else{
 		var query = {};
@@ -50,14 +64,14 @@ function findObject(Model, param, uniqueIdentifier, res, cb){
 }
 
 module.exports = function(router, route, ModelPath, config) {
-
+	
 	//Initial configurations
 	var Model = require(ModelPath);
 	var securityFilter;
 	if (_.isUndefined(config.security)){
-		securityFilter = function(/*req, MethodType*/){
+		securityFilter = function(req, MethodType){
 			return true;
-		};
+		}
 	}
 	else{
 		securityFilter = require(config.security);
@@ -65,28 +79,44 @@ module.exports = function(router, route, ModelPath, config) {
 
 	//LIST
 	router.get(route, function(req, res) {
-		Model.find(function(err, objects) {
+
+		var query = {}
+		query = readQuery(query, Model, req.query)
+		Model.find(query, function(err, objects) {
 			if (securityFilter(objects, req, "LIST")){
 				if (err)
 					res.send(err);
-
 				res.json(objects);
-			}
-			else {
-				res.status(403);
-				res.send('Acesso não autorizado');
-			}
+				}
+				else {
+					res.status(403);
+					res.send('Acesso não autorizado');
+				}
 		});
 	});
-
 	//CREATE
 	router.post(route, function(req, res) {
-
+	
 		var object = new Model();
-
+	
 		object = readRequest(object, Model, req);
 
-		if (securityFilter(object, req, "POST")){
+		/*dummyuser = new User()
+		dummyuser.name="Dummy"
+		dummyuser.age =4
+		dummyuser.save(function(err, saved) {
+			if(err){
+				console.log(err)
+				res.send(err);
+			}
+			else {
+				console.log('Modelo salvo! \n', object);
+				res.status(200).json({SUCCESS: object});
+			}
+		}).then(object.creator=dummyuser);*/
+
+		
+		if (securityFilter(object, req, "POST")){	
 			saveModel(object, res);
 		}
 		else {
@@ -94,10 +124,11 @@ module.exports = function(router, route, ModelPath, config) {
 			res.send('Acesso não autorizado');
 		}
 	});
-
+	
 	//READ
 	router.get(route + '/:UniqueIdentifier', function(req, res){
 		findObject(Model, req.params.UniqueIdentifier, config.uniqueIdentifier, res, function(object){
+			// ESTUDAR O USO DE POPULATE PRA PREENCHER TODOS OS CAMPOS NECESSARIOS.
 			if (securityFilter(object, req, "GET")){
 				res.send(object);
 			}
@@ -107,7 +138,7 @@ module.exports = function(router, route, ModelPath, config) {
 			}
 
 		});
-	});
+	});	
 
 
 	//UPDATE
@@ -127,7 +158,7 @@ module.exports = function(router, route, ModelPath, config) {
 	//DELETE
 	router.delete(route + '/:UniqueIdentifier', function(req,res){
 		findObject(Model, req.params.UniqueIdentifier, config.uniqueIdentifier, res, function(object){
-			if (securityFilter(req, "DELETE")){
+			if (securityFilter(object, req, "DELETE")){
 				object.remove(function(err) {
 					if(err) {
 						res.json({ERROR: err});
@@ -146,4 +177,4 @@ module.exports = function(router, route, ModelPath, config) {
 	});
 
 	return router;
-};
+}
